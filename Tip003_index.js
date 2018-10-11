18f62db99c817ac4cb5ed8b34e72b2945d15e3b2
Susana Ruiz <susana.r.cunado@gmail.com>
2018-10-11 10:03:49 +0200

    Añadidos comentarios y más ejemplos

--- Tip003_index.js
+++ Tip003_index.js
@@ -1,10 +1,10 @@
-import './style.scss';
-import './editor.scss';
-import icon from './icon';
+//Puedes añadir tu código svg en un archivo aparte para que tu código sea más mantenible
+//Sólo tienes que hacer referencia a "icono" en la propiedad icon de registerBlockType 
+//import icono from './icon';
 
 // Constante con icono en forma svg
-const blockIcon = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6 6h-6v-6h6v6zm9-6h-6v6h6v-6zm9 0h-6v6h6v-6zm-18 9h-6v6h6v-6zm9 0h-6v6h6v-6zm9 0h-6v6h6v-6zm-18 9h-6v6h6v-6zm9 0h-6v6h6v-6zm9 0h-6v6h6v-6z"/></svg>;
-// Ejemplo simple con un circulo
+const blockIcon = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path d="M6 6h-6v-6h6v6zm9-6h-6v6h6v-6zm9 0h-6v6h6v-6zm-18 9h-6v6h6v-6zm9 0h-6v6h6v-6zm9 0h-6v6h6v-6zm-18 9h-6v6h6v-6zm9 0h-6v6h6v-6zm9 0h-6v6h6v-6z"/></svg>;
+// Ejemplo simple con un circulo svg
 //<svg width="20" height="20"><circle cx="10" cy="10" r="8" stroke="black" strokeWidth="2" fill="white" /></svg>;
 
 const { __ } = wp.i18n;
@@ -23,11 +23,15 @@ export default registerBlockType(
         description: __('Código de ejemplo decomo personalizar un icono con svg en tu bloque'),
         // En icon ponemos el nombre de la variable que hemos declarado arriba
         icon: blockIcon,
+        // Tambien puedes añadir el codigo svg directamente pero pierdes legibilidad, mejor constante o archivo aparte
+        //icon: <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path d="M6 6h-6v-6h6v6zm9-6h-6v6h6v-6zm9 0h-6v6h6v-6zm-18 9h-6v6h6v-6zm9 0h-6v6h6v-6zm9 0h-6v6h6v-6zm-18 9h-6v6h6v-6zm9 0h-6v6h6v-6zm9 0h-6v6h6v-6z"/></svg>,
+        // Si has incluido un archivo con tu código svg no olvides importarlo arriba y referenciarlo en icon
+        //icon: icono, 
         keywords: [
             __( 'Prueba' ),
             __( 'Primer Ejemplo' ),
             __( 'Bloque ejemplo' ),
-        ],        
+        ],
         attributes: {
             content: {
                 type: 'array',
